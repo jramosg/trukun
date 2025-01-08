c9a88da7643ac3fc102b1f44afd72f8558efdbb5
Jon <jonurnieta@gmail.com>
2025-01-08 11:19:30 +0100

    wip

--- src/clj/kit/trukun/web/middleware/log.clj
+++ src/clj/kit/trukun/web/middleware/log.clj
@@ -4,5 +4,5 @@
 
 (defn wrap-log [handler]
   (fn [request]
-    (log/info ::request (select-keys request [:uri :request-method :header :headers ]))
+    (log/info ::request (select-keys request [:uri :request-method :header :headers :cookies]))
     (handler request)))
