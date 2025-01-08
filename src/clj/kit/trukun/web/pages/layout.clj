6a8cd47b52f1cec144d3f804e2cb717a21c8522b
Jon <jonurnieta@gmail.com>
2025-01-08 19:15:43 +0100

    wip

--- src/clj/kit/trukun/web/pages/layout.clj
+++ src/clj/kit/trukun/web/pages/layout.clj
@@ -3,7 +3,7 @@
    [clojure.java.io]
    [hiccup2.core :as h]
    [kit.trukun.config :refer [system]]
-   [ring.middleware.anti-forgery :refer [*anti-forgery-token*]] 
+   [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [ring.util.http-response :refer [content-type ok]]
    [ring.util.response :as response]
@@ -22,7 +22,7 @@
   (response/set-cookie resp "x-csrf-token"
                        *anti-forgery-token*
                        {:http-only "true"
-                        :path"/"
+                        :path "/"
                         :secure "true"
                         :max-age 86400
                         :same-site :none}))
@@ -31,14 +31,15 @@
   [_request template & [params]]
   (log/info "render home" {:api-url (:trukun/api-url @system)})
   (->  (parser/render-file template
-                          (assoc params :page template :csrf-token *anti-forgery-token*)
-                          selmer-opts)
-      (content-type "text/html; charset=utf-8")
-      (add-anti-forgery-token)))
+                           (assoc params :page template :csrf-token *anti-forgery-token*)
+                           selmer-opts)
+       (ok)
+       (content-type "text/html; charset=utf-8")
+       (add-anti-forgery-token)))
 
 (comment
   ;; = = - Example 1 = 
-  
+
   user=> (= 1)
   true
   user=> (= 1 1)
@@ -55,11 +56,11 @@
   true
   user=> (= (sorted-set 2 1) (sorted-set 1 2))
   true
-  
+
   ;; It should be noted that equality is not defined for Java arrays.
   ;; Instead you can convert them into sequences and compare them that way.
   ;; (= (seq array1) (seq array2))
-  
+
   ;; See also:
   clojure.core/==
   clojure.core/not=
@@ -79,5 +80,5 @@
    :body    (parser/render-file "error.html" error-details selmer-opts)})
 
 
-(comment 
+(comment
   (str *anti-forgery-token*))
